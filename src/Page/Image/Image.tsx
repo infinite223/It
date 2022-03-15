5bcb26ed10326c8b5f1b9affbe4534a0960111b5
infinite223 <dawidszmigiel9@gmail.com>
2022-03-15 11:10:13 +0100

    sticy navbar and more

--- src/Page/Image/Image.tsx
+++ src/Page/Image/Image.tsx
@@ -7,7 +7,7 @@ function Image() {
     <div className='Image flex'>
         <div className='Image-left flex'>
             <h1>We focus on ensuring a competitive edge</h1>
-            <img width="400px" src={image} alt="picture9"/>
+            <img width="350px" src={image} alt="picture9"/>
         </div>
         <div className='Image-right flex'>
         <p>Article evident arrived express highest men did boy. Mistress sensible entirely am so. Quick can manor smart money hopes worth too. Comfort produce husband boy her had hearing. Law others theirs passed but wishes.
--- src/Page/Navbar/Navbar.tsx
+++ src/Page/Navbar/Navbar.tsx
@@ -1,10 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {GiHamburgerMenu} from 'react-icons/gi';
 import { IoCloseOutline} from 'react-icons/io5'
+import { motion } from 'framer-motion';
 import './Navbar.scss'
 
 function Navbar() {
   const [toggleMenu,setToggleMenu] = useState(false);
+
+  const [scrolled,setScrolled]=React.useState(false);
+  const handleScroll=() => {
+    const offset=window.scrollY;
+    if(offset > 200 ){
+      setScrolled(true);
+    }
+    else{
+      setScrolled(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll',handleScroll)
+  })
+  let navbarClasses=['Navbar'];
+  if(scrolled){
+    navbarClasses.push('scrolled');
+  }
 
   return (
     <div className='Navbar'>
@@ -16,13 +36,17 @@ function Navbar() {
       </div>
       
       <div className='Navbar__menu-smallscreen'>
-        <GiHamburgerMenu color='black' fontSize={27} onClick={()=>setToggleMenu(true)}/>
-        {toggleMenu&&<div className='Navbar__menu-options-smallscreen'>
+        <GiHamburgerMenu color='white' fontSize={27} onClick={()=>setToggleMenu(true)}/>
+        {toggleMenu&&<motion.div 
+           animate={{width:[10,25,50,100,150,200,240,240,250]}}
+           transition={{ duration: .3 }}    
+        className='Navbar__menu-options-smallscreen'
+        >
           <IoCloseOutline fontSize={27} className='close' onClick={()=>setToggleMenu(false)}/>
           <div className='Navbar__menu-link-small'>Home</div>
           <div className='Navbar__menu-link-small'>About</div>
           <div className='Navbar__menu-link-small'>Contact</div>
-        </div>}
+        </motion.div>}
       </div>
 
     </div>
