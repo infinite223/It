54504a656a983d6679d56120422c3ee5fc2c5557
infinite223 <dawidszmigiel9@gmail.com>
2022-04-24 23:16:22 +0200

    new changes

--- src/Page/Main/Flexible/Numbers/Numbers.tsx
+++ src/Page/Main/Flexible/Numbers/Numbers.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Numbers.scss'
+import CountUp from 'react-countup';
+
+import VisibilitySensor from 'react-visibility-sensor';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content" />
+      <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+        {({ isVisible }) => (
+          <div style={{ height: 100 }}>
+            {isVisible ? <CountUp end={1000} /> : null}
+          </div>
+        )}
+      </VisibilitySensor>
+    </div>
+  );
+}
 
 export const Numbers = ({number,text}) => {
   return (
-    <div className='Numbers flex'>
-        <h2>{number}</h2>
+    <div className='Numbers flex' onBlur={()=>console.log("elo")} onFocus={()=>console.log("elo")}>
+        <h2>{number}{App()}</h2>
         <h3>{text}</h3>
         <hr style={{width:"30%",height:"3px",color:"white",border:"0px",backgroundColor:"white"}}/>
     </div>
